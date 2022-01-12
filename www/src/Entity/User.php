8fc7b2d384673941eedc1ab7cbbf8b5ec490f9f8
JS <julian.saleix@gmail.com>
2022-01-12 19:46:16 +0100

    Adding user registering & authenticating

--- www/src/Entity/User.php
+++ www/src/Entity/User.php
@@ -3,15 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
  */
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -21,137 +21,79 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=110)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
-    private $name;
+    private $email;
 
     /**
-     * @ORM\Column(type="string", length=80)
+     * @ORM\Column(type="json")
      */
-    private $surname;
+    private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=110)
-     */
-    private $mail;
-
-    /**
-     * @ORM\Column(type="string", length=255)
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
     private $password;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $avatar;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $city;
-
-    /**
-     * @ORM\Column(type="string", length=12, nullable=true)
-     */
-    private $phone;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $joinDate;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isActive;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $idStripeConnect;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Role::class, inversedBy="users")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $role;
-
-    /**
-     * @ORM\OneToMany(targetEntity=ProductReport::class, mappedBy="reporter", orphanRemoval=true)
-     */
-    private $productReports;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="buyer", orphanRemoval=true)
-     */
-    private $invoices;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Sneaker::class, mappedBy="publisher")
-     */
-    private $sneakers;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=UserReport::class, inversedBy="reporter")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $userReports;
-
-    public function __construct()
-    {
-        $this->productReports = new ArrayCollection();
-        $this->invoices = new ArrayCollection();
-        $this->sneakers = new ArrayCollection();
-        $this->userReports = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getEmail(): ?string
     {
-        return $this->name;
+        return $this->email;
     }
 
-    public function setName(string $name): self
+    public function setEmail(string $email): self
     {
-        $this->name = $name;
+        $this->email = $email;
 
         return $this;
     }
 
-    public function getSurname(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->surname;
+        return (string) $this->email;
     }
 
-    public function setSurname(string $surname): self
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
     {
-        $this->surname = $surname;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getMail(): ?string
-    {
-        return $this->mail;
-    }
-
-    public function setMail(string $mail): self
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -163,223 +105,23 @@ class User
         return $this;
     }
 
-    public function getAvatar(): ?string
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
     {
-        return $this->avatar;
-    }
-
-    public function setAvatar(?string $avatar): self
-    {
-        $this->avatar = $avatar;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getPhone(): ?string
-    {
-        return $this->phone;
-    }
-
-    public function setPhone(?string $phone): self
-    {
-        $this->phone = $phone;
-
-        return $this;
-    }
-
-    public function getJoinDate(): ?\DateTimeInterface
-    {
-        return $this->joinDate;
-    }
-
-    public function setJoinDate(\DateTimeInterface $joinDate): self
-    {
-        $this->joinDate = $joinDate;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->isActive;
-    }
-
-    public function setIsActive(bool $isActive): self
-    {
-        $this->isActive = $isActive;
-
-        return $this;
-    }
-
-    public function getIdStripeConnect(): ?string
-    {
-        return $this->idStripeConnect;
-    }
-
-    public function setIdStripeConnect(?string $idStripeConnect): self
-    {
-        $this->idStripeConnect = $idStripeConnect;
-
-        return $this;
-    }
-
-    public function getRole(): ?Role
-    {
-        return $this->role;
-    }
-
-    public function setRole(?Role $role): self
-    {
-        $this->role = $role;
-
-        return $this;
+        return null;
     }
 
     /**
-     * @return Collection|ProductReport[]
+     * @see UserInterface
      */
-    public function getProductReports(): Collection
+    public function eraseCredentials()
     {
-        return $this->productReports;
-    }
-
-    public function addProductReport(ProductReport $productReport): self
-    {
-        if (!$this->productReports->contains($productReport)) {
-            $this->productReports[] = $productReport;
-            $productReport->setReporter($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProductReport(ProductReport $productReport): self
-    {
-        if ($this->productReports->removeElement($productReport)) {
-            // set the owning side to null (unless already changed)
-            if ($productReport->getReporter() === $this) {
-                $productReport->setReporter(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Invoice[]
-     */
-    public function getInvoices(): Collection
-    {
-        return $this->invoices;
-    }
-
-    public function addInvoice(Invoice $invoice): self
-    {
-        if (!$this->invoices->contains($invoice)) {
-            $this->invoices[] = $invoice;
-            $invoice->setBuyer($this);
-        }
-
-        return $this;
-    }
-
-    public function removeInvoice(Invoice $invoice): self
-    {
-        if ($this->invoices->removeElement($invoice)) {
-            // set the owning side to null (unless already changed)
-            if ($invoice->getBuyer() === $this) {
-                $invoice->setBuyer(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Sneaker[]
-     */
-    public function getSneakers(): Collection
-    {
-        return $this->sneakers;
-    }
-
-    public function addSneaker(Sneaker $sneaker): self
-    {
-        if (!$this->sneakers->contains($sneaker)) {
-            $this->sneakers[] = $sneaker;
-            $sneaker->setPublisher($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSneaker(Sneaker $sneaker): self
-    {
-        if ($this->sneakers->removeElement($sneaker)) {
-            // set the owning side to null (unless already changed)
-            if ($sneaker->getPublisher() === $this) {
-                $sneaker->setPublisher(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getUserReports(): ?UserReport
-    {
-        return $this->userReports;
-    }
-
-    public function setUserReports(?UserReport $userReports): self
-    {
-        $this->userReports = $userReports;
-
-        return $this;
-    }
-
-    public function addUserReport(UserReport $userReport): self
-    {
-        if (!$this->userReports->contains($userReport)) {
-            $this->userReports[] = $userReport;
-            $userReport->setReportedUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeUserReport(UserReport $userReport): self
-    {
-        if ($this->userReports->removeElement($userReport)) {
-            // set the owning side to null (unless already changed)
-            if ($userReport->getReportedUser() === $this) {
-                $userReport->setReportedUser(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
