50802d41eb3771bd0b4d3986806fd022ad0206c6
Pierre <Victsing@outlook.fr>
2022-01-13 15:34:15 +0100

    remove stimulus bootstrap

--- www/assets/app.js
+++ www/assets/app.js
@@ -7,7 +7,3 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-import './bootstrap';
-import bsCustomFileInput from "bs-custom-file-input";
-// start the Stimulus application
-bsCustomFileInput.init()
