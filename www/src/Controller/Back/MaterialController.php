e40367c4e80ebccac3ca4bdb64044372912cf79e
JS <julian.saleix@gmail.com>
2022-01-13 12:31:55 +0100

    Adding admin Material crud

--- www/src/Controller/Back/MaterialController.php
+++ www/src/Controller/Back/MaterialController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/material')]
+#[Route('/admin/ressources/materials')]
 class MaterialController extends AbstractController
 {
     #[Route('/', name: 'material_index', methods: ['GET'])]
--- www/src/Form/MaterialType.php
+++ www/src/Form/MaterialType.php
@@ -15,7 +15,6 @@ class MaterialType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('icon')
-            ->add('sneakers')
         ;
     }
 
