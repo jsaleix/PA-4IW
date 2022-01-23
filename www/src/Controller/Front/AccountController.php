3544f666c28b899606b2cb14245f763bdc05638e
JS <julian.saleix@gmail.com>
2022-01-23 14:49:17 +0100

    Edit profile works

---
+++ www/src/Controller/Front/AccountController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Front\UserType;
+
+#[Route('/account')]
+class AccountController extends AbstractController
+{
+    #[Route('/', name: 'account_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('front/account/index.html.twig', []);
+    }
+
+    #[Route('/profile', name: 'account_profile', methods: ['GET', 'POST'])]
+    public function profile(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('front_account_profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('front/account/profile/index.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+         ]);
+    }
+}
---
+++ www/src/Form/Front/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Front;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('avatar')
+            ->add('city')
+            ->add('address')
+            ->add('phone')
+            ->add('name')
+            ->add('surname')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
