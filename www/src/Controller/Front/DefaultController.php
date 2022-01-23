f53abec422c37648b451ad2293a92f9d870b1bae
JS <julian.saleix@gmail.com>
2022-01-23 15:56:50 +0100

    Home page started

--- www/src/Controller/Front/DefaultController.php
+++ www/src/Controller/Front/DefaultController.php
@@ -6,14 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Finder\Finder;
+use App\Repository\CategoryRepository;
+use App\Repository\BrandRepository;
+use App\Repository\SneakerRepository;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, BrandRepository $brandRepository, SneakerRepository $sneakerRepository): Response
     {        
-        return $this->render('Front/default/index.html.twig', [
-            'controller_name' => 'FRONT',
+        
+        return $this->render('front/default/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+            'brands' => $brandRepository->findBy(array(), array(), 4, 0),
+            'popularSneakers' => $sneakerRepository->findBy(array(), array(), 4, 0),
         ]);
     }
 }
