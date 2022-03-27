96890a71710fb2e60cd9296bc1126131b519e396
JS <julian.saleix@gmail.com>
2022-03-27 15:39:02 +0200

    Adding ReportController + ProductReport & UseReport form types

---
+++ www/src/Controller/Front/ReportController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\ProductReport;
+use App\Entity\ReportReason;
+use App\Entity\Sneaker;
+use App\Entity\User;
+use App\Entity\UserReport;
+use App\Form\Front\ProductReportFormType;
+use App\Form\Front\UserReportType;
+use App\Repository\ReportReasonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ReportController extends AbstractController
+{
+    #[Route('/report/user/{id}', name: 'report-user')]
+    public function reportUser(User $user, Request $request, EntityManagerInterface $entityManager, ReportReasonRepository $reasonRepository): Response
+    {
+        $reasons = $reasonRepository->findBy(['type' => '1']);
+        $report = new UserReport();
+        $form = $this->createForm( UserReportType::class, $report);
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid() ){
+            try{
+
+            }catch(\Exception $e){
+
+            }
+        }
+        return $this->render('front/report/report_user.html.twig', [
+            'form' => $form->createView(),
+            'reasons' => $reasons
+        ]);
+    }
+
+    #[Route('/report/sneaker/{id}', name: 'report-sneaker')]
+    public function reportSneaker(Sneaker $sneaker, Request $request, EntityManagerInterface $entityManager, ReportReasonRepository $reasonRepository): Response
+    {
+        $reasons = $reasonRepository->findBy(['type' => '2']);
+        $report = new ProductReport();
+        $form = $this->createForm( ProductReportFormType::class, $report);
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid() ){
+            try{
+                dd($sneaker);
+            }catch(\Exception $e){
+
+            }
+        }
+        return $this->render('front/report/report_sneaker.html.twig', [
+            'form' => $form->createView(),
+            'reasons' => $reasons
+        ]);
+    }
+}
---
+++ www/src/Form/Front/ProductReportFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\Front;
+
+use App\Entity\ProductReport;
+use App\Entity\ReportReason;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductReportFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('reason', EntityType::class, [
+                'class' => ReportReason::class,
+                'choice_label' => 'name',
+                'multiple' => false
+            ])
+            ->add('Submit', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ProductReport::class,
+        ]);
+    }
+}
