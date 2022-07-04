56e8c3e1878e31c6a3da7c0ce057260793d1e6f6
JS <julian.saleix@gmail.com>
2022-07-04 16:32:27 +0200

    Adding webpack + sassloader + navbar starting in scss

--- www/config/bundles.php
+++ www/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
