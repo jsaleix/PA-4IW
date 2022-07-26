08cb551028e0260ff4b87b15f0edd5e44652d5b2
jsaleix <julian.saleix@gmail.com>
2022-07-27 01:10:41 +0200

    Testing adding new routes for stripe webhook

--- www/src/Controller/WebhookController.php
+++ www/src/Controller/WebhookController.php
@@ -21,8 +21,12 @@ use App\Service\Payment\PaymentService;
 #[Route('/webhook')]
 class WebhookController extends AbstractController
 {
-    #[Route('/stripe', name: 'stripe_listener', methods: ['POST'])]
-    public function index(
+
+    const ACCOUNT_EVENT_TYPES = ['payment_intent.succeeded', 'checkout.session.expired', 'payment_intent.canceled'];
+    const CONNECT_EVENT_TYPES = ['account.updated', 'capability.updated'];
+
+    #[Route('/stripe/dev', name: 'stripe_listener_dev', methods: ['POST'])]
+    public function dev(
         Request $request, 
         UserRepository $userRepository,
         InvoiceRepository $invoiceRepository,
@@ -85,37 +89,111 @@ class WebhookController extends AbstractController
         }
     }
 
-    // #[Route('/stripe/{id}', name: 'stripe_event_visualizer', requirements: ['id' => '^\d+$'], methods: ['GET'])]
-    // public function event(
-    //     Request $request,
-    //     $id,
-    //     InvoiceRepository $invoiceRepository,
-    //     LoggerInterface $logger
-    // ): Response
-    // {
-    //     $logger->info('I just got the logger');
+    #[Route('/stripe/connect', name: 'stripe_listener_connect', methods: ['POST'])]
+    public function connectEvent(
+        Request $request, 
+        UserRepository $userRepository,
+        InvoiceRepository $invoiceRepository,
+        EntityManagerInterface $entityManager,
+        PaymentService $paymentService,
+        SellerService $sellerService,
+        LoggerInterface $logger
+    ): Response
+    {
+        try{
+            //$stripe= new StripeClient($_ENV['STRIPE_SK']);
+            $webhookSecret = $_ENV['STRIPE_WH_SK_CONNECT'];
+            $signature = $request->headers->get('stripe-signature');
 
-    //     $stripe= new StripeClient($_ENV['STRIPE_SK']);
-    //     $event = $stripe->events->retrieve($id);
-    //     $stripePI = $event['data']['object']['id'];
-    //     //dd($stripePI);
-    //     $invoice = $invoiceRepository->findOneBy(['stripePI' => $stripePI]);
-    //     //dd($invoice);
+            if( !$webhookSecret) { throw new \Exception('Missing secret'); }
+            $event = \Stripe\Webhook::constructEvent(
+                $request->getcontent(),
+                $signature,
+                $webhookSecret
+            );
 
-    //     return new JsonResponse([$event]);
-    // }
+            $type   = $event['type'];
+            $object = $event['data']['object'];
 
-    // #[Route('/stripe/transfers', name: 'stripe_transfers_visualizer', methods: ['GET'])]
-    // public function transfersList(
-    //     InvoiceRepository $invoiceRepository,
-    //     PaymentService $paymentService
-    // ): Response
-    // {
-    //     $invoice = $invoiceRepository->findOneBy(['id' => 13]);
-    //     //$test = $paymentService->confirmPaymentMP($invoice);
-    //     $stripe= new StripeClient($_ENV['STRIPE_SK']);
-    //     $transfers = $stripe->transfers->all(['limit' => 3]);
+            switch ($type) {
+                case 'account.updated':
+                case 'capability.updated':
+                    $sellerService->updateSellerCapabilities($event);
+                    break;
+                
+                default:
+                    throw new \Exception('Unhandled event');
+            }
+            
+            $response = new Response();
+            $response->setStatusCode(Response::HTTP_OK);
+            return $response;
+            
+        } catch (\Exception $e) {
+            $logger->error($e);
+            $response = new Response();
+            $response->setStatusCode(Response::HTTP_FORBIDDEN);
+            return $response;
+        }
+    }
 
-    //     return new JsonResponse([]);
-    // }
+    #[Route('/stripe/account', name: 'stripe_listener_account', methods: ['POST'])]
+    public function account(
+        Request $request, 
+        UserRepository $userRepository,
+        InvoiceRepository $invoiceRepository,
+        EntityManagerInterface $entityManager,
+        PaymentService $paymentService,
+        SellerService $sellerService,
+        LoggerInterface $logger
+    ): Response
+    {
+        try{
+            //$stripe= new StripeClient($_ENV['STRIPE_SK']);
+            $webhookSecret = $_ENV['STRIPE_WH_SK'];
+            $signature = $request->headers->get('stripe-signature');
+
+            if( !$webhookSecret) { throw new \Exception('Missing secret'); }
+            $event = \Stripe\Webhook::constructEvent(
+                $request->getcontent(),
+                $signature,
+                $webhookSecret
+            );
+
+            $type   = $event['type'];
+            $object = $event['data']['object'];
+
+            switch ($type) {
+                case 'payment_intent.succeeded':
+                    //Retrieving invoice
+                    $invoice = $invoiceRepository->findOneBy(['stripePI' => $object['id']]);
+                    if(!$invoice) throw new \Exception('No invoice linked');
+                    //creates bill
+                    $paymentService->confirmPayment($invoice);
+                    break;
+
+                case 'checkout.session.expired':
+                case 'payment_intent.canceled':
+                    //Retrieving invoice
+                    $invoice = $invoiceRepository->findOneBy(['stripePI' => $object['id']]);
+                    if(!$invoice) throw new \Exception('No invoice linked');
+                    $paymentService->removeInvoice($invoice);
+                    break;
+                
+                default:
+                    throw new \Exception('Unhandled event');
+            }
+            
+            $response = new Response();
+            $response->setStatusCode(Response::HTTP_OK);
+            return $response;
+            
+        } catch (\Exception $e) {
+            $logger->error($e);
+            $response = new Response();
+            $response->setStatusCode(Response::HTTP_FORBIDDEN);
+            return $response;
+        }
+    }
+
 }
